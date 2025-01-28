cc0dd7f02cbad95325ee134bd4048d3ec00bec91
Neil Local <neil.joseph.tacatani@accenture.com>
2025-01-28 18:51:46 +0800

    very small changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user.id, role: user.role },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: '15m' }
+    { expiresIn: '60m' }
   );
 };
 
